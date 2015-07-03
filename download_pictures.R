5392e1af4605cfb8c3c9b7ba56c9f604afd04683
basbaccarne <bastiaan.baccarne@ugent.be>
2015-07-03 08:40:51 +0200

    other FB pages

--- download_pictures.R
+++ download_pictures.R
@@ -1,7 +1,17 @@
-data <- read.csv("../R/links.csv")
+data <- read.csv("DMI facebook img - lovewins.csv")
 
 for (i in 1:nrow(data)){
-        download.file(as.character(data[i,1]), paste("celebratepride", i, ".jpg", sep=""), method="auto", mode = 'wb')
+        download.file(as.character(data[i,1]), paste("FB_lovewins", i, ".jpg", sep=""), method="auto", mode = 'wb')
 }
+
+
+names <- data.frame()
+
+for (i in 1:nrow(data)){
+        rbind(names,(paste("FB_lovewins", i, ".jpg", sep="")
+}
+
+names <- paste("FB_lovewins", 1:nrow(data), ".jpg", sep="")
+ write.csv(names, "names.csv")
 
 
